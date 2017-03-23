b6cf4983225e72db354314ce34fbb32380a25295
Danilo Díaz Tarascó <danilodt@gmail.com>
2017-03-23 10:09:39 -0400

    Update with 2 relay

--- ESP8266_Firmware/ESP8266_Firmware.ino
+++ ESP8266_Firmware/ESP8266_Firmware.ino
@@ -1,13 +1,3 @@
-/*
- *  EasyloT sketch tweeked by Barnabybear - original @
- *  http://iot-playground.com/2-uncategorised/40-esp8266-wifi-relay-switch-arduino-ide
- *  This sketch demonstrates how to set up a simple HTTP-like server.
- *  The server will set a GPIO pin depending on the request
- *    http://server_ip/gpio/0 will set the GPIO2 low,
- *    http://server_ip/gpio/1 will set the GPIO2 high
- *  server_ip is the IP address of the ESP8266 module, will be 
- *  printed to Serial when the module is connected.
- */
 
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h> 
@@ -28,6 +18,10 @@ void handleRoot() {
 
 void setup() {
   Serial.println("Inicio de servidor");
+  Serial.print("SSID: ");
+  Serial.print(ssid);
+  Serial.print("Password: ");
+  Serial.println(password);
   initHardware();
   setupWiFi();
   server.begin();
@@ -44,13 +38,7 @@ void loop() {
     delay(100);
     return;
   }
-  
-  // Commented out by BB as gets stuck in while loop.
-  // Wait until the client sends some data
-  //Serial.println("new client");
-  //while(!client.available()){
-  //  delay(1);
-  //}
+
   
   // Read the first line of the request
   
@@ -59,13 +47,23 @@ void loop() {
   client.flush();
   
   // Match the request
-  int val;
-  if (req.indexOf("/gpio/0") != -1)
-    val = 0;
-  else if (req.indexOf("/gpio/1") != -1)
-    val = 1;
+  int val1;
+  int val2;
+  if (req.indexOf("/gpio/0") != -1){
+    val1 = 0;
+    val2 = 0;
+  }
+   
+  else if (req.indexOf("/gpio/1") != -1){
+    val1 = 1;
+    val2 = 1;
+  }
+    
   else if (req.indexOf("/gpio/2") != -1)
-   val = (!digitalRead(2)); // <<<<< Edit: insert /gpio/3 lines after this line.
+   val1 = (!digitalRead(2)); 
+   else if (req.indexOf("/gpio/3") != -1)
+   val2 = (!digitalRead(0));
+   
   else {
     Serial.println("invalid request");
     client.print("HTTP/1.1 404\r\n");
@@ -73,24 +71,23 @@ void loop() {
     return;
   }
 
-  // Set GPIO2 according to the request
-  digitalWrite(2, val);
+  // Set GPIOX according to the request
+  digitalWrite(2,val1);
+  digitalWrite(0,val2);
   
   client.flush();
 
   // Prepare the response
-  String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\nGPIO is now ";
+  String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\nRelé está en";
   s += (val)?"high":"low";
   s += "</html>\n";
 
   // Send the response to the client
   client.print(s);
   delay(1);
-  Serial.println("Client disonnected");
+  Serial.println("Cliente desconectado");
 
-  // The client will actually be disconnected 
-  // when the function returns and 'client' object is detroyed
-} //- See more at: http://www.esp8266.com/viewtopic.php?f=11&t=6211#sthash.FDIZvYhb.dpuf
+
 
 
 void setupWiFi()
@@ -121,5 +118,8 @@ void initHardware()
   //pinMode(DIGITAL_PIN, INPUT_PULLUP);
   pinMode(2, OUTPUT);
   digitalWrite(2, LOW);
+
+  pinMode(0, OUTPUT);
+  digitalWrite(0, LOW);
 }
 
